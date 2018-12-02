4021e29a277f15ac3c79a303ab7a99fef2c6d2e9
Volodya Solohub <vsolohub71@gmail.com>
2018-12-02 16:58:43 +0200

    test

--- lab_6/task1.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('NYC_Jobs.csv', sep=',')
-print(df)
--- lab_6/task3.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-df = pd.read_csv('NYC_Jobs.csv')
-names = ['Agency', '# Of Positions']
-print(df[names])
-df['Agency'].value_counts(sort = True).plot.bar()
-plt.show()
